d2bd20d40f3444e5b01f7fa2a8e2201b26534b4a
OrinTD <orintchicken933@gmail.com>
2026-08-26 19:18:53 +0000

    Goon

--- CokeCooler.java
+++ CokeCooler.java
@@ -1,3 +1,18 @@
 public class CokeCooler {
-    
+    private int cokes;
+    public CokeCooler(int c){
+        cokes = c;
+    }
+    public CokeCooler(){
+        cokes = 100;
+    }
+    public void drink(int d){
+        cokes -= d;
+    }
+    public void add(int a){
+        cokes += a;
+    }
+    public void cokeCount(){
+        System.out.println("There are " + cokes + " cokes in the cooler.");
+    }
 }
---
+++ CokeCoolerMain.java
@@ -0,0 +1,9 @@
+public class CokeCoolerMain {
+    public static void main(String[] args){
+        CokeCooler c = new CokeCooler(99999999);
+        c.cokeCount();
+        c.drink(99999998);
+        c.add(11);
+        c.cokeCount();
+    }
+}
