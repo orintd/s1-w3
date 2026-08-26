93d7aff90f75bb3e6dde12cb5013b8a4a5669005
orintd <abcwhattheheck123@gmail.com>
2026-08-26 18:35:30 +0000

    LeBruh James

---
+++ CandyJarMain.java
@@ -0,0 +1,12 @@
+public class CandyJarMain {
+    public static void main(String[] args) {
+        CandyJar j = new CandyJar();
+        CandyJar j1 = new CandyJar(69420);
+        j.printState();
+        j1.printState();
+        j.eat(3000);
+        j1.refill(999999999);
+        j.printState();
+        j1.printState();
+    }
+}
