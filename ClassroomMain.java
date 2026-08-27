be975e265d4fbc88ad5a2ff656652fea660d9823
Orin TD <orintchicken933@gmail.com>
2026-08-26 21:20:28 -0700

    Monke

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,5 +1,7 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        
+        Classroom c = new Classroom(30);
+        Classroom c1 = new Classroom("Davidson", 32, 34);
+        c1.printState();
     }
 }
