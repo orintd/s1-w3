6ce4014ae818feb894b645a15d1dcaff4b655d07
Orin TD <orintchicken933@gmail.com>
2026-08-28 17:22:36 +0000

    Goon

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,7 +1,10 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        Classroom c = new Classroom(30);
+        Classroom c = new Classroom("Bruh");
+        c.setDesks(30);
         Classroom c1 = new Classroom("Davidson", 32, 34);
-        c1.printState();
+        System.out.println("Teacher: " + c1.teacher());
+        System.out.println("Number of desks: " + c1.desks());
+        System.out.println("Number of students: " + c1.students());
     }
 }
